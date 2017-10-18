d126a261af600b65e7e7d83808a941db1e94262e
Florian Kostenzer <florian.kostenzer@icloud.com>
2017-10-18 16:36:48 +0200

    Update query.php

--- core/process/query.php
+++ core/process/query.php
@@ -4,4 +4,3 @@ $variables = realpath(dirname(__FILE__)) . '/../json/variables.json';
 $config = json_decode(file_get_contents($variables));
 
 include_once('queries/' . ($config->system->db_type ?: 'rocketmap') . '.php');
-include_once('queries/' . ($config->system->db_type ?: 'monocle') . '.php');
