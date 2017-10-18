b4e8011e570a91d44ef7c4af182df255a8959af3
Demokdawa <Demokdawa@yahoo.fr>
2017-10-18 16:18:59 +0200

    Db-trigger

--- core/process/query.php
+++ core/process/query.php
@@ -4,3 +4,4 @@ $variables = realpath(dirname(__FILE__)) . '/../json/variables.json';
 $config = json_decode(file_get_contents($variables));
 
 include_once('queries/' . ($config->system->db_type ?: 'rocketmap') . '.php');
+include_once('queries/' . ($config->system->db_type ?: 'monocle') . '.php');
